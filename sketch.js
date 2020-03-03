429203efe9477799106bf6f3f06434218f80b623
Heru Wang <heru.wang@sjsu.edu>
2020-03-02 19:37:57 -0800

    first
    draft

--- sketch.js
+++ sketch.js
@@ -1,22 +1,55 @@
 //fruits I love
 
-let fruits = [{name:"strawberry", color:"red"},{name: "blueberry", color: "blue"}{name:"cherry", color:"dark red"} {name: "grape", color:"purple"}{name: "mango", color:"yellow"}];
+let fruits = [{
+  name: "strawberry",
+  color: "red"
+},{
+  name: "blueberry",
+  color: "blue"
+}, {
+  name: "cherry",
+  color: "dark red"
+},{
+  name: "grape",
+  color: "purple"
+}, {
+  name: "mango",
+  color: "yellow"
+}];
+
+let randomIndex
+let animating = true;
 
 function setup() {
   createCanvas(600, 600);
   background(200);
-
-  console.log("initial array is");
-  console.log(fruits);
-
-
-  fruits.splice(2, 2);
-  console.log("array after splice");
-  console.log(fruits);
-
+  textSize(32);
+  text("click to randomizer",50,50);
 
 }
 
 function draw() {
+  if(animating == true){
+    ellipse(random(width), random(height), random(50, 200));
+  }
+}
+
+function randomizer(){
+  animating = false;
+   if(fruits[0]){
+   background(random(200,255));
+   randomIndex = int(random(fruits.length));
+     console.log(fruits[randomIndex].name);
+     text(`${fruits[randomIndex].name}'s color is ${fruits[randomIndex].color}`,50,50);
+     fruits.splice(randomIndex,1)
+   }else{
+     background(random(200,255));
+     text("nothing left!", 50,50);
+   }
+}
+function mousePressed(){
+
+  animating = true;
+  setTimeout(randomizer, 2000);
 
 }
