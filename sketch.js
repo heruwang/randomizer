0aaaa66fee3c8d67f12c081091e51bc0e5020565
Heru Wang <heru.wang@sjsu.edu>
2020-03-09 16:55:04 -0700

    button
    button set up

--- sketch.js
+++ sketch.js
@@ -1,53 +1,82 @@
-//fruits I love
+//years in the future
 
-let fruits = [{
-  name: "strawberry",
-  color: "red"
+let years = [{
+  name: "2022",
 },{
-  name: "blueberry",
-  color: "blue"
+  name: "2025",
 }, {
-  name: "cherry",
-  color: "dark red"
+  name: "2028",
 },{
-  name: "grape",
-  color: "purple"
+  name: "2030",
 }, {
-  name: "mango",
-  color: "yellow"
+  name: "2033",
 }];
 
 let randomIndex
 let animating = true;
+let futures = [];
+let imageCounter=0;
+let button;
+function preload(){
+  for(let i = 0; i<=6; i++){
+    futures[i] = loadImage('assets/future' + i + '.jpeg');
+  }
+}
 
 function setup() {
-  createCanvas(600, 600);
-  background(200);
-  textSize(32);
+   createCanvas(1000, 1000);
+  textSize(36);
+  imageMode(CENTER);
+  textAlign(CENTER);
+  textStyle(BOLD);
+  frameRate(8);
   text("click to randomizer",50,50);
+
+  button = createButton('click to see your future');
+  button.mousePressed(buttonPressed);
+  button.class("randomizerButton");
 
 }
 
 function draw() {
+  background(220);
+
+
+  // background(bg);
+  // stroke(226, 204, 0);
+  // eclipse(width/2, height/2,x)
+  // x++;
+  // if(x > height)
+  // {
+  //   x=0;
+  // }
+
   if(animating == true){
-    ellipse(random(width), random(height), random(50, 200));
+    clear();
+    image(futures[imageCounter],width/2, height/2)};
+
+    if(imageCounter < futures.length-1)
+    {imageCounter++;}
+    else{imageCounter = 0;}
   }
-}
 
 function randomizer(){
   animating = false;
-   if(fruits[0]){
-   background(random(200,255));
-   randomIndex = int(random(fruits.length));
-     console.log(fruits[randomIndex].name);
-     text(`${fruits[randomIndex].name}'s color is ${fruits[randomIndex].color}`,50,50);
-     fruits.splice(randomIndex,1)
+   if(years[0]){
+   //background(random(200,255));
+   clear();
+   randomIndex = int(random(years.length));
+
+    image(random(futures),width/2, height/2);
+     text(`year = ${years[randomIndex].name}`,width/2, height - 25);
+     years.splice(randomIndex,1)
    }else{
      background(random(200,255));
      text("nothing left!", 50,50);
    }
 }
-function mousePressed(){
+
+function buttonPressed(){
 
   animating = true;
   setTimeout(randomizer, 2000);
